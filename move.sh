08de6612391c8fe90387f03394ab12d814eabfa9
Yukijya Kuroda <bic_a_k@icloud.com>
2023-08-23 21:45:24 +0900

    nothing

--- move.sh
+++ move.sh
@@ -3,7 +3,7 @@
 function main(){
     find $1 -empty -delete
     find $1 -name \*.out -delete
-    # mv ~/Atcoder/$1 ~/Atcoder/AtCoder_Beginner_Contests
+    mv ~/Atcoder/$1 ~/Atcoder/AtCoder_Beginner_Contests
 }
 
 main $1
